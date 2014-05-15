8b55abb143267a4289dbab82f3d075c22f76808d
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2014-05-15 17:56:11 -0400

    Add license blocks to source files

--- deltaic/source.py
+++ deltaic/source.py
@@ -1,3 +1,22 @@
+#
+# Deltaic - an efficient backup system supporting multiple data sources
+#
+# Copyright (c) 2014 Carnegie Mellon University
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 from datetime import datetime, date
 import os
 import Queue
