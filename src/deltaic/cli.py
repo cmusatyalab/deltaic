87daab438939999b33ef409570fa0f2f3af1245a
Jan Harkes <jaharkes@cs.cmu.edu>
2021-12-09 22:39:24 -0500

    Add --version flag

--- src/deltaic/cli.py
+++ src/deltaic/cli.py
@@ -39,6 +39,7 @@ CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 
 @with_plugins(iter_entry_points("deltaic.lowlevel"))
 @click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option()
 @click.option(
     "-c",
     "--config-file",
