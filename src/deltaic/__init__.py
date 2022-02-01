8cd70a05be2a308531a1f212160c31654748a25e
Jan Harkes <jaharkes@cs.cmu.edu>
2022-01-31 23:17:24 -0500

    Update dependencies + bump minimal python version to 3.7

--- src/deltaic/__init__.py
+++ src/deltaic/__init__.py
@@ -4,6 +4,5 @@
 # example from https://docs.python.org/3/library/pkgutil.html
 
 from pkgutil import extend_path
-from typing import List
 
-__path__: List[str] = extend_path(__path__, __name__)
+__path__ = extend_path(__path__, __name__)
